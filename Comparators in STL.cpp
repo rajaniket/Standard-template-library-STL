4ada08a80e9b78d3d068727b6d58215c9bfa80dc
Aniket Raj <aniketraj.go@gmail.com>
2020-05-29 23:58:54 +0530

    Create Comparators in STL.cpp

---
+++ Comparators in STL.cpp
@@ -0,0 +1,27 @@
+// How Comparators function works in sorting
+#include<iostream>
+#include<algorithm>
+bool comparator(int a,int b){
+return b<a;
+}
+using namespace std;
+// defining bubble sorting function
+void Bubble_Sort(int *arr,int n,bool (&cp)(int a,int b)){ // comparator function as an argument 
+for(int i=0;i<n;i++)
+for(int j=1;j<n-i;j++){
+    if(cp(arr[j],arr[j-1]))  // it will work on the basis of comparator function output 
+        swap(arr[j-1],arr[j]);
+}
+}
+int main(){
+int arr[]={80,3,66,5,2,77,2,100,54};
+    int n=sizeof(arr)/sizeof(arr+n);
+ for(int i=0;i<n;i++){
+        cout<<arr[i]<<"  ";
+    }
+    Bubble_Sort(arr,n,comparator);
+    cout<<"\nAfter sorting"<<endl;
+      for(int i=0;i<n;i++){
+        cout<<arr[i]<<"  ";
+    }
+}
